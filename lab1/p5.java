761974442591b0ba60e27937e63fe36203defd8c
AlexBlagescu <44620859+AlexBlagescu@users.noreply.github.com>
2020-02-27 19:48:38 +0200

    Create p5.java

---
+++ lab1/p5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class p5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        
+        int numberOfDiv = 0;
+        
+        for(int i = 1; i <= a; i++) {
+            if(a % i == 0)
+                numberOfDiv++;
+        }
+        if(numberOfDiv == 2)
+            System.out.println("Numarul este prim");
+        else 
+            System.out.println("Numarul nu este prim");
+    }
+}
